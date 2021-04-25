9da34720d6c33198ed17d0f5a874bbbc2017fc56
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-04-25 14:56:50 -0500

    objects lecture

---
+++ objects/objects-lecture.js
@@ -0,0 +1,50 @@
+/*** OBJECTS ***/
+
+
+/** INTRODUCTION **/
+
+// // What is an object?
+
+
+//  // New Object Instance
+// // Object is an existing js keyword
+// // we can create _instances_ of that object to create custom objects
+    console.log("object: " + Object);
+    typeof (Object);
+
+    var faveFood = new Object();
+    console.log(faveFood);
+
+
+//  // Object Literal:
+var myFirstObject = {
+    date: "April 28, 2021",
+    name: "Laura",
+    age: 30,
+    favoriteFoods: ['pizza', 'mango', 'coffee ice cream'],
+    livesInJapan: false
+}
+
+console.log(myFirstObject);
+
+// // Setting properties within object
+
+
+// // Accessing Information within Objects
+// // DOT NOTATION
+// // ARRAY NOTATION
+
+// // Nested Values
+
+// // Assigning Functionality
+
+// // THIS keyword
+
+
+
+
+
+
+
+
+/** MATH **/
