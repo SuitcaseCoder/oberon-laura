219df8ff8bbf966d31bca5c2c3e564fc99bf6ccc
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-04-27 08:05:25 -0500

    objects-lecture

--- objects/objects-lecture.js
+++ objects/objects-lecture.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*** OBJECTS ***/
 
 
@@ -45,21 +47,132 @@
 
 // // what's the difference between dot and bracket (array) notation?
 
+// // Avoid doing this:
+
+
+// should instead be:
+
+
+
+
 // // Accessing Information within Objects
 // // DOT NOTATION
+
+// // return just the value of the spurs' coach
+
+
+
 // // ARRAY NOTATION
+
+
 
 // // Nested Values
 
+
+
 // // Assigning Functionality
+
 
 // // THIS keyword
 
 
-
-
-
+/** Mini Exercises *//
+/*  TODO: Create a zoo object that contains basic info about the zoo, as well as, information
+      about 5 different animals. For each animal in your object, create a method that returns
+       the sound of that animal using the THIS keyword
+       hint: Each animal should have at least 5 properties (one of them being animalSound);
+*/
 
 
 
 /** MATH **/
+
+// // Math.random();
+
+// random num between 0 and 1;
+
+
+// now let's remove the decimals using toFixed
+
+
+//  now let's get a whole number between 20 and 40
+
+
+// let's break it down:
+
+
+// // TODO:
+
+// // Math.round();
+
+// // Math.floor();
+
+
+// // Math.ceil();
+
+// // Math.pow();
+
+
+// // Math.sqrt();
+
+
+// // Math Constants
+
+// // Euler's Number
+
+// // Pi
+
+// // Question: what's an example of a Math property and what's an example of a Math method?
+// // What's the difference?
+
+
+/** Mini Exercises **/
+
+// TODO: Write a function that returns a random team from the given array:
+var teams = ["76ers",
+    "Bucks" ,
+    "Bulls" ,
+    "Cavaliers" ,
+    "Celtics" ,
+    "Clippers" ,
+    "Grizzlies" ,
+    "Hawks" ,
+    "Heat" ,
+    "Hornets" ,
+    "Jazz" ,
+    "Kings" ,
+    "Knicks" ,
+    "Lakers" ,
+    "Magic" ,
+    "Mavericks" ,
+    "Nets" ,
+    "Nuggets" ,
+    "Pacers" ,
+    "Pelicans" ,
+    "Pistons" ,
+    "Raptors" ,
+    "Rockets" ,
+    "Spurs" ,
+    "Suns" ,
+    "Thunder" ,
+    "Timberwolves" ,
+    "Trail Blazers" ,
+    "Warriors" ,
+    "Wizards"]
+console.log(teams);
+
+// // solution:
+function getRandomTeam(arr) {
+
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    // get random item
+    const item = arr[randomIndex];
+
+    return item;
+}
+
+
+const result = getRandomTeam(teams);
+console.log(result);
