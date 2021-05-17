fef6d96351012c67673f4b8b4e04505777b3664f
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-05-17 13:50:45 -0500

    bootstrap design

---
+++ warmups/warmups.js
@@ -0,0 +1,12 @@
+// Write a function named secondToLast(arr) that returns the data type of
+// the second to last element of the array parameter. When you call the
+// function, pass in an array of your choosing
+
+var nombres = ['juan', 'jose', 'pablo','pedro','jorge'];
+
+function secondToLast(arr){
+//    loop through array
+//
+}
+
+secondToLast(nombres);
