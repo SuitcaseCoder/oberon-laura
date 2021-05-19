fdb6451b0bdb0b728d407b3ecb389ad5da7a542b
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-05-19 02:44:17 -0500

    dom

---
+++ DOM/dom-lecture/script.js
@@ -0,0 +1,45 @@
+// retrieving elements
+
+// by id
+console.log(document.getElementById("red"));
+
+// what's returned?
+
+
+// by className
+
+// what's returned?
+//
+
+
+// get by Tag Name
+// get ALL elements
+
+// what's returned?
+
+
+
+
+// // TODO: build a simple 2 x 3 grid using bootstrap and give each
+//  item a unique border color. Give any three of those of those divs a
+//  blue background-color. Place a paragraph tag inside of all 6 divs
+//  (use lorem). Four of the six paragraphs should have dark-green font
+//  color
+
+// TODO: print the following items to the console: 1) A node that
+//  represents a div/item with a red border. 2) an array of elements
+//  whose text is dark-green. 3) an array of all paragraph elements
+
+// TODO: bonus: log the second paragraph element from the list of all
+//  paragraphs
+
+// ADDING ELEMENTS
+
+
+// style elements
+
+// FORMS
+
+// get inpt values
+console.log(document.forms.pizza.topping.value);
+
---
+++ DOM/index.js
@@ -0,0 +1,99 @@
+// flipping cards
+
+// ---- PASTA
+function flipPasta() {
+    console.log("clicked");
+    //remove hide class on flipped card
+    document.getElementById("pasta-flipped").removeAttribute("class");
+    //add show class on flipped card
+    document.getElementById("pasta-flipped").setAttribute("class", "col-3 d-block");
+    // remove show class on card
+    document.getElementById("pasta").removeAttribute("class");
+    //add hide class on card
+    document.getElementById("pasta").setAttribute("class", "col-3 d-none");
+
+};
+
+function flipPastaBack(){
+    console.log("clicked");
+    //remove hide class on flipped card
+    document.getElementById("pasta-flipped").removeAttribute("class");
+    //add show class on flipped card
+    document.getElementById("pasta-flipped").setAttribute("class", "col-3 d-none");
+    // remove show class on card
+    document.getElementById("pasta").removeAttribute("class");
+    //add hide class on card
+    document.getElementById("pasta").setAttribute("class", "col-3 d-block");
+}
+
+
+// ---- BED
+function flipSleep() {
+    console.log("clicked");
+    //remove hide class on flipped card
+    document.getElementById("bed-flipped").removeAttribute("class");
+    //add show class on flipped card
+    document.getElementById("bed-flipped").setAttribute("class", "col-3" +
+        " d-block");
+    // remove show class on card
+    document.getElementById("bed").removeAttribute("class");
+    //add hide class on card
+    document.getElementById("bed").setAttribute("class", "col-3 d-none");
+
+};
+
+function flipSleepBack(){
+    console.log("clicked");
+    //remove hide class on flipped card
+    document.getElementById("bed-flipped").removeAttribute("class");
+    //add show class on flipped card
+    document.getElementById("bed-flipped").setAttribute("class", "col-3" +
+        " d-none");
+    // remove show class on card
+    document.getElementById("bed").removeAttribute("class");
+    //add hide class on card
+    document.getElementById("bed").setAttribute("class", "col-3 d-block");
+}
+
+// ---- FAST
+function flipFast() {
+    console.log("clicked");
+    //remove hide class on flipped card
+    document.getElementById("fast-flipped").removeAttribute("class");
+    //add show class on flipped card
+    document.getElementById("fast-flipped").setAttribute("class", "col-3" +
+        " d-block");
+    // remove show class on card
+    document.getElementById("fast").removeAttribute("class");
+    //add hide class on card
+    document.getElementById("fast").setAttribute("class", "col-3 d-none");
+
+};
+
+function flipFastBack(){
+    console.log("clicked");
+    //remove hide class on flipped card
+    document.getElementById("fast-flipped").removeAttribute("class");
+    //add show class on flipped card
+    document.getElementById("fast-flipped").setAttribute("class", "col-3" +
+        " d-none");
+    // remove show class on card
+    document.getElementById("fast").removeAttribute("class");
+    //add hide class on card
+    document.getElementById("fast").setAttribute("class", "col-3 d-block");
+}
+
+
+
+// pasta
+document.getElementById("pasta").addEventListener("click", flipPasta);
+document.getElementById("pasta-flipped").addEventListener("click", flipPastaBack);
+
+// bed
+document.getElementById("bed").addEventListener("click", flipSleep);
+document.getElementById("bed-flipped").addEventListener("click", flipSleepBack);
+
+// fast
+document.getElementById("fast").addEventListener("click", flipFast);
+document.getElementById("fast-flipped").addEventListener("click", flipFastBack);
+
