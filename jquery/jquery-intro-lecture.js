c8ce331754d227c1342dff12a4407bb345470ef9
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-06-01 07:58:34 -0500

    jQuery

--- jquery/jquery-intro-lecture.js
+++ jquery/jquery-intro-lecture.js
@@ -1,7 +1,14 @@
+"use strict";
+
 // Intro to jQuery
 
+// setup jQuery
+// jquery - download
+// jquery - cdn
+
 // syntax
-//
+
+// syntax - select by className
 
 // syntax - selecting by id
 
@@ -9,3 +16,16 @@
 
 // mini exercise:
 
+// window.onload
+window.onload = function(){
+    alert("The window has finished loading");
+    console.log("--- window done loading at: ----");
+    console.log(new Date());
+}
+
+// document.ready
+$(document).ready(function(){
+    alert("the DOM has finished loading");
+    console.log("--- DOM finished loading at: ---");
+    console.log(new Date());
+});
