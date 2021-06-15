924d8e6dcce5db5fd10f8795964c15dde649dcc7
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-06-15 10:45:10 -0500

    promises

---
+++ javascript-ii/promises.js
@@ -0,0 +1,25 @@
+// JS Promises
+
+// what is a promise in js
+
+// what is async vs sync
+
+// three states of a promise
+
+// Fetch
+
+// basic fetch syntax
+
+
+// let's break down what .then() method does
+
+// fetch syntax with .catch() method
+
+
+// let's break down what .catch() method does
+
+
+// fetch syntax for post request
+
+// chaining
+
