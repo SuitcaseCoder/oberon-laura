82a86f6de38a3719e7e645e4090eb26b1ff7b151
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-06-27 23:49:35 -0500

    String Method Examples

--- java-i/src/Strings.java
+++ java-i/src/Strings.java
@@ -1,8 +1,24 @@
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Strings {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+//        warmup:
+//        TODO: create a new "Strings-Lecture" java class file (or wherever
+//         you like to keep notes/lecture exercises. Copy given
+//         code, that is your starting point. Using Scanner prompt a
+//         user (in this case it's you, you're the user) to type in
+//         their first name. Save the input into a String variable. If
+//         your name matches randomName, print "woah! you guessed:
+//         enter-randomName-here! How did you know enter-yourName-here
+//         is my name?!". if randomName does
+//         not
+//         match your name exactly, print "Way off my friend! My name
+//         isn't randomName-here. It's yourName-here.".
+
 
 //        Intro
 
@@ -18,10 +34,34 @@ public class Strings {
 
 //        String Comparison
 //        .equalsIgnoreCase()
-        
+
 //        .startsWith()
 
 //        .endsWith()
 
+
+//        STRING MANIPULATION  METHODS
+
+//        charAt():
+
+//        indexOf():
+
+//        lastIndexOf():
+
+//        length():
+
+//        replace():
+
+//        substring():
+
+//        toLowerCase();
+
+        
+//        toUpperCase():
+
+//        trim():
+
+    
+    
     }
 }
