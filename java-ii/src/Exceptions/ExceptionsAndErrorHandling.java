98a1417be02d6394717e80c5461bb44c1b594778
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2021-07-12 23:02:11 -0500

    Exceptions

---
+++ java-ii/src/Exceptions/ExceptionsAndErrorHandling.java
@@ -0,0 +1,20 @@
+package Exceptions;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExceptionsAndErrorHandling {
+    public static void main(String[] args) {
+        
+        // COMMON EXCEPTION EXAMPLES
+        // //parseInt Exception
+
+        
+        // // ArrayList Exception
+
+//        CHECK ACCOUNT ELIGIBILITY
+
+    }
+    
+    // CHECK ELIGIBILITY METHOD
+}
